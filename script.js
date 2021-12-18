70a08600455e1e247f2626fad2c046f397022725
Hasnaat <25773007+HasnaatCodes@users.noreply.github.com>
2021-12-18 15:26:38 +0000

    Refactored the code

--- script.js
+++ script.js
@@ -1,75 +1,52 @@
-const playerOneButton = document.querySelector('#playerOne')
-const playerTwoButton = document.querySelector('#playerTwo')
-const resetButton = document.querySelector('#reset')
-const playerOneDisplay = document.querySelector('#playerOneDisplay')
-const playerTwoDisplay = document.querySelector('#playerTwoDisplay')
-const scoreSelection = document.querySelector('#topscore');
+const playerOne = {
+    score: 0,
+    button: document.querySelector('#playerOne'),
+    display: document.querySelector('#playerOneDisplay'),
+}
 
-let playerOneScore = 0;
-let playerTwoScore = 0;
+const playerTwo = {
+    score: 0,
+    button: document.querySelector('#playerTwo'),
+    display: document.querySelector('#playerTwoDisplay'),
+}
+
+const resetButton = document.querySelector('#reset')
+const scoreSelection = document.querySelector('#topscore');
 let maxScore = 5;
 
-scoreSelection.addEventListener('change', (event) => {
-    maxScore = getMaxScore(event.target.value);
+scoreSelection.addEventListener('change', function () {
+    maxScore = parseInt(this.value);
+    reset();
 });
 
-playerOneButton.addEventListener("click", function (event) {
-    playerOneScore++;
-    playerOneDisplay.textContent = playerOneScore;
+playerOne.button.addEventListener("click", function () {
+    updateScores(playerOne, playerTwo);
+});
 
-    if (playerOneScore >= maxScore) {
-        playerOneDisplay.style.color = 'green';
-        playerTwoDisplay.style.color = 'red';
+playerTwo.button.addEventListener("click", function () {
+    updateScores(playerTwo, playerOne);
+});
 
-        playerOneButton.disabled = true;
-        playerTwoButton.disabled = true;
+resetButton.addEventListener("click", reset);
+
+function updateScores(player, opponent) {
+    player.score++;
+    player.display.textContent = player.score;
+
+    if (player.score >= maxScore) {
+        player.display.style.color = 'green';
+        player.button.disabled = true;
+
+        opponent.display.style.color = 'red';
+        opponent.button.disabled = true;
     }
-});
+}
 
-playerTwoButton.addEventListener("click", function (event) {
-    playerTwoScore++;
-    playerTwoDisplay.textContent = playerTwoScore;
-
-    if (playerTwoScore >= maxScore) {
-        playerTwoDisplay.style.color = 'green';
-        playerOneDisplay.style.color = 'red';
-
-        playerOneButton.disabled = true;
-        playerTwoButton.disabled = true;
-    }
-});
-
-reset.addEventListener("click", function (event) {
-    playerOneScore = 0;
-    playerTwoScore = 0;
-
-    playerOneDisplay.textContent = playerOneScore;
-    playerTwoDisplay.textContent = playerTwoScore;
-
-    maxScore = getMaxScore(scoreSelection.options[scoreSelection.selectedIndex].value)
-    playerTwoDisplay.style.color = 'black';
-    playerOneDisplay.style.color = 'black';
-
-    playerOneButton.disabled = false;
-    playerTwoButton.disabled = false;
-
-});
-
-function getMaxScore(value) {
-    switch (value) {
-        case "five":
-            return 5;
-        case "six":
-            return 6;
-        case "seven":
-            return 7;
-        case "eight":
-            return 8;
-        case "nine":
-            return 9;
-        case "ten":
-            return 10;
-        default:
-            break;
+function reset() {
+    for (let player of [playerOne, playerTwo]) {
+        player.score = 0;
+        player.display.textContent = 0;
+        player.display.style.color = 'black';
+        player.button.disabled = false;
     }
 }
